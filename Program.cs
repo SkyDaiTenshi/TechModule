c159fafc6c3c8b75408a7da50127da60067dfd02
SkyDaiTenshi <holyone@abv.bg>
2017-01-27 20:44:40 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,71 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FallenInLove
+namespace PerfectGirlfriend
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var n = int.Parse(Console.ReadLine());
-            var counter1 = 1;
-            var countern = n;
-            var countern2 = n*2;
-            //1vi red
 
-            Console.Write("##");
-            Console.Write(new string ('.',n*2));
-            Console.Write("##");
-            Console.Write(new string('.', n * 2));
-            Console.Write("##");
-            Console.WriteLine();
-            //chashkata na cveteto do predi centyra i
-
-            for (int i=1; i<n; i++)
-
+            String Input = string.Empty;
+            int counter = 0;
+            while (Input != "Enough dates!")
             {
-                Console.Write('#');
-                Console.Write(new string('~', i));
-                Console.Write('#');
-                Console.Write(new string('.', (n*2)-(i*2)));
-                Console.Write('#');
-                Console.Write(new string('.', (i * 2)));
-                Console.Write('#');
-                Console.Write(new string('.', (n * 2) - (i * 2)));
-                Console.Write('#');
-                Console.Write(new string('~', i));
-                Console.Write('#');
-                Console.WriteLine();
-            }
+                Input = Console.ReadLine();
 
-            // ot centyra na dolu
+                if (Input == "Enough dates!")
+                {
+                    Console.WriteLine(counter);
+                    break;
+                }
 
-            for (int i=1; i<=n+1; i++)
-            {
-                Console.Write(new string('.', counter1));
-                Console.Write('#');
-                Console.Write(new string('~', countern));
-                Console.Write('#');
-                Console.Write(new string('.', countern2));
-                countern2 -= 2;
-                Console.Write('#');
-                Console.Write(new string('~', countern));
-                countern -= 1;
-                Console.Write('#');
-                Console.Write(new string('.', counter1));
-                counter1 += 2;
-                Console.WriteLine();
-            }
+                else
+                {
 
-            for (int i=1; i<=n; i++)
-            {
-                Console.Write(new string('.', ((4*n+6)/2)-1));
-                Console.Write("##");
-                Console.Write(new string('.', ((4 * n + 6) / 2) - 1));
-                Console.WriteLine();
+                    string[] part = Input.Split('\\');
 
-            }
+                    var DayNumber = new Dictionary<string, int>();
+
+                    DayNumber.Add("Monday", 1);
+                    DayNumber.Add("Tuesday", 2);
+                    DayNumber.Add("Wednesday", 3);
+                    DayNumber.Add("Thursday", 4);
+                    DayNumber.Add("Friday", 5);
+                    DayNumber.Add("Saturday", 6);
+                    DayNumber.Add("Sunday", 7);
+
+                    int DayInt = DayNumber[part[0]];
+                    int PhoneInt = 0;
+                    string phonenumber = part[1];
+
+
+                    for (int i = 0; i <= phonenumber.Length - 1; i++)
+                    {
+                        int currentdigit = 0;
+                        int.TryParse(phonenumber[i] + "", out currentdigit);
+                        PhoneInt += currentdigit;
+                    }
+                    string BraNumber = string.Empty;
+                    String Bra = part[2];
+                    int BraInt = 0;
+
+                    for (int i = 0; i <= part[2].Length - 2; i++)
+                    {
+                        BraNumber += Bra[i];
+                    }
+                    int.TryParse(BraNumber, out BraInt);
+
+
+                    int BraLetter = Bra[Bra.Length - 1];
+                    BraInt = BraInt * ((int)BraLetter);
+
+                    int NameInt = 0;
+                    string Name = part[3];
+                    NameInt += (int)Name[0];
+                    NameInt = NameInt * Name.Length;
+
+                    int Result = DayInt + PhoneInt + BraInt - NameInt;
+
+                    if (Result >= 6000)
+                    {
+                        Console.WriteLine("{0} is perfect for you.", Name);
+                        counter++;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Keep searching, {0} is not for you.", Name);
+                    }
+                }
+        }        
         }
     }
 }
-
